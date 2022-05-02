367435a9eee4351ba0f5aa7eab7575cd65794dde
Alavish <achristopher22@my.whitworth.edu>
2022-05-02 14:08:44 -0700

    First commit

---
+++ src/components/app/pages/questions/index.tsx
@@ -0,0 +1,41 @@
+/** @format */
+import React, { useState, useEffect } from "react";
+import Page from "../page";
+import styles from "./styles.scss";
+import QuestionToolBar from "../../../questions-toolbar";
+import QuestionWriterUI from "../../../questions-box";
+import { Exam, loadPartialExam } from "../../../../model/Exam";
+import { useLocation, useParams } from "react-router-dom";
+
+const Questions = () => {
+	const location = useLocation();
+	const [exam, setExam] = useState<Exam | undefined>();
+
+	useEffect(() => {
+		loadPartialExam(parseInt(location.state as string)).then(
+			(loadedExam) => {
+				loadedExam
+					.downloadExistingQuestionsIfNecessary()
+					.then((actualExam) => {
+						setExam(actualExam);
+					});
+			},
+		);
+	}, []);
+
+	return (
+		<Page>
+			{/* <div className={styles.questionsBox}>
+				<QuestionWriterUI />
+			</div>
+			<div className={styles.questionsToolBar}></div> */}
+			<div className={styles.titleBar}>{exam?.name}</div>
+			{/* <div className={styles.questionsToolBar}>
+				<QuestionToolBar />
+			</div> */}
+			{/* {exam != undefined ? <DragUIContainer exam={exam} /> : ""} */}
+		</Page>
+	);
+};
+
+export default Questions;
---
+++ src/components/exam-titlebar/index.tsx
@@ -0,0 +1,13 @@
+/** @format */
+import React from "react";
+import styles from "./styles.scss";
+
+const ExamTitleBar = () => {
+	return (
+		<div className={styles.examTitleBar}>
+			<div className={styles.examName}> </div>
+		</div>
+	);
+};
+
+export default ExamTitleBar;
